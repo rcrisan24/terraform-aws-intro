1e6d299806ac7bd624c33ff8112f6703bc85ded2
Raoul Crisan <raoul.crisan@qubiz.com>
2022-08-01 17:21:20 +0300

    gandalf added

---
+++ gandalf/variables.tf
@@ -0,0 +1,31 @@
+variable "ecs_task_execution_role" {
+  
+}
+
+variable "ecs_task_role" {
+  
+}
+
+variable "ecs_cluster_id" {
+  
+}
+
+variable "public_subnets_ids" {
+  
+}
+
+variable "target_group_arn" {
+  
+}
+
+variable "name" {
+    
+}
+
+variable "security_group_id" {
+
+}
+
+variable "lb_listener" {}
+
+variable "service_name" {}
